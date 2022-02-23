fd35e7276b6e08b864907955cfde258feafc8ba0
Major <golubin.alex@gmail.com>
2022-02-23 23:58:46 +0300

    Доработал калькулятор

--- Lesson3/HomeWork/calc/main.go
+++ Lesson3/HomeWork/calc/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -11,11 +12,13 @@ func main() {
 	fmt.Print("Введите первое число: ")
 	fmt.Scanln(&a)
 
-	fmt.Print("Введите арифметическую операцию (+, -, *, /): ")
+	fmt.Print("Введите арифметическую операцию (+, -, *, /, ^, !): ")
 	fmt.Scanln(&op)
 
-	fmt.Print("Введите второе число: ")
-	fmt.Scanln(&b)
+	if op != "!" {
+		fmt.Print("Введите второе число: ")
+		fmt.Scanln(&b)
+	}
 
 	switch op {
 	case "+":
@@ -25,11 +28,34 @@ func main() {
 	case "*":
 		res = a * b
 	case "/":
+		if b == 0 {
+			fmt.Println("Деление на 0 невозможно.")
+			return
+		}
 		res = a / b
+	case "^":
+		res = float32(math.Pow(float64(a), float64(b)))
+	case "!":
+		if (a < 0) || (a != float32(int32(a))) {
+			fmt.Println("Расчитать факториал можно только с неотрицательными целыми числами")
+			return
+		}
+		res = float32(calcFactorial(int(a)))
 	default:
 		fmt.Println("Операция выбрана неверно")
 		return
 	}
 
-	fmt.Printf("Результат выполнения операции: %f\n", res)
+	if res == float32(int32(res)) {
+		fmt.Printf("Результат выполнения операции: %d\n", int32(res))
+	} else {
+		fmt.Printf("Результат выполнения операции: %.2f\n", res)
+	}
+}
+
+func calcFactorial(number int) int {
+	if number < 2 {
+		return 1
+	}
+	return number * calcFactorial(number-1)
 }
