7afe94f79f0a3477a6fd4fa11dcf9ff165685ee3
Major <golubin.alex@gmail.com>
2022-02-23 23:26:31 +0300

    Исправил кавычки

--- Lesson2/HomeWork/rectangle/main.go
+++ Lesson2/HomeWork/rectangle/main.go
@@ -8,14 +8,14 @@ func main() {
 	var a int
 	var b int
 
-	fmt.Print("Введите длину стороны \"a\": ")
+	fmt.Print(`Введите длину стороны "a": `)
 	_, err := fmt.Scanln(&a)
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Print("Введите длину стороны \"b\": ")
+	fmt.Print(`Введите длину стороны "b": `)
 	_, err = fmt.Scanln(&b)
 
 	if err != nil {
