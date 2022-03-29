c624e08d0324b1754a273d609b3fd48b0835915a
Major <golubin.alex@gmail.com>
2022-03-29 23:59:08 +0300

    Чистка кода

--- Lesson8/HomeWork/config/config.go
+++ Lesson8/HomeWork/config/config.go
@@ -19,75 +19,58 @@ type Config struct {
 }
 
 func FromFlags() (Config, error) {
-	port := flag.Int("port", 0, "Номер порта")
-	dbUrlRaw := flag.String("db-url", "", "URL подключения к БД")
-	jaegerUrlRaw := flag.String("jaeger-url", "", "URL подключения к Jaeger")
-	sentryUrlRaw := flag.String("sentry-url", "", "URL подключения к Sentry")
+	port := flag.String("port", "", "Номер порта")
+	dbUrl := flag.String("db-url", "", "URL подключения к БД")
+	jaegerUrl := flag.String("jaeger-url", "", "URL подключения к Jaeger")
+	sentryUrl := flag.String("sentry-url", "", "URL подключения к Sentry")
 	kafkaBroker := flag.String("kafka-broker", "", "Сервис Kafka")
 	someAppId := flag.String("some-app-id", "", "Some app id")
 	someAppKey := flag.String("some-app-key", "", "Some app key")
 
 	flag.Parse()
 
-	dbUrl, err := parseUrl(*dbUrlRaw, "db-url")
-	if err != nil {
-		return Config{}, err
-	}
-	jaegerUrl, err := parseUrl(*jaegerUrlRaw, "jaeger-url")
-	if err != nil {
-		return Config{}, err
-	}
-	sentryUrl, err := parseUrl(*sentryUrlRaw, "sentry-url")
-	if err != nil {
-		return Config{}, err
-	}
-
-	return Config{
-		Port:        *port,
-		DbUrl:       dbUrl,
-		JaegerUrl:   jaegerUrl,
-		SentryUrl:   sentryUrl,
-		KafkaBroker: *kafkaBroker,
-		SomeAppId:   *someAppId,
-		SomeAppKey:  *someAppKey,
-	}, nil
+	return FromRaw(*port, *dbUrl, *jaegerUrl, *sentryUrl, *kafkaBroker, *someAppId, *someAppKey)
 }
 
 func FromEnv() (Config, error) {
-	portRaw := os.Getenv("PORT")
-	dbUrlRaw := os.Getenv("DB_URL")
-	jaegerUrlRaw := os.Getenv("JAEGER_URL")
-	sentryUrlRaw := os.Getenv("SENTRY_URL")
+	port := os.Getenv("PORT")
+	dbUrl := os.Getenv("DB_URL")
+	jaegerUrl := os.Getenv("JAEGER_URL")
+	sentryUrl := os.Getenv("SENTRY_URL")
 	kafkaBroker := os.Getenv("KAFKA_BROKER")
 	someAppId := os.Getenv("SOME_APP_ID")
 	someAppKey := os.Getenv("SOME_APP_KEY")
 
-	if portRaw == "" {
+	return FromRaw(port, dbUrl, jaegerUrl, sentryUrl, kafkaBroker, someAppId, someAppKey)
+}
+
+func FromRaw(port, dbUrl, jaegerUrl, sentryUrl, kafkaBroker, someAppId, someAppKey string) (Config, error) {
+	if port == "" {
 		return Config{}, errors.New("Необходимо указать порт")
 	}
-	port, err := strconv.Atoi(portRaw)
+	portInt, err := strconv.Atoi(port)
 	if err != nil {
 		return Config{}, err
 	}
 
-	dbUrl, err := parseUrl(dbUrlRaw, "db-url")
+	dbUrlStruct, err := parseUrl(dbUrl, "db-url")
 	if err != nil {
 		return Config{}, err
 	}
-	jaegerUrl, err := parseUrl(jaegerUrlRaw, "jaeger-url")
+	jaegerUrlStruct, err := parseUrl(jaegerUrl, "jaeger-url")
 	if err != nil {
 		return Config{}, err
 	}
-	sentryUrl, err := parseUrl(sentryUrlRaw, "sentry-url")
+	sentryUrlStruct, err := parseUrl(sentryUrl, "sentry-url")
 	if err != nil {
 		return Config{}, err
 	}
 
 	return Config{
-		Port:        port,
-		DbUrl:       dbUrl,
-		JaegerUrl:   jaegerUrl,
-		SentryUrl:   sentryUrl,
+		Port:        portInt,
+		DbUrl:       dbUrlStruct,
+		JaegerUrl:   jaegerUrlStruct,
+		SentryUrl:   sentryUrlStruct,
 		KafkaBroker: kafkaBroker,
 		SomeAppId:   someAppId,
 		SomeAppKey:  someAppKey,
--- Lesson8/HomeWork/main.go
+++ Lesson8/HomeWork/main.go
@@ -6,21 +6,27 @@ import (
 )
 
 func main() {
-	// go run main.go --port=8080 --db-url="postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable" --jaeger-url=http://jaeger:16686 --sentry-url=http://sentry:9000 --kafka-broker=kafka:9092 --some-app-id=testid --some-app-key=testkey
-
-	//config, err := config.FromFlags()
+	// go run main.go
+	//configRaw, err := config.FromRaw("8080", "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable", "http://jaeger:16686", "http://sentry:9000", "kafka:9092", "testid", "testkey")
 	//if err != nil {
 	//	panic(err)
 	//}
 	//
-	//fmt.Println(config)
+	//fmt.Println(configRaw)
+
+	// go run main.go --port=8080 --db-url="postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable" --jaeger-url=http://jaeger:16686 --sentry-url=http://sentry:9000 --kafka-broker=kafka:9092 --some-app-id=testid --some-app-key=testkey
+	//configFlags, err := config.FromFlags()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println(configFlags)
 
 	// PORT=8080 DB_URL="postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable" JAEGER_URL=http://jaeger:16686 SENTRY_URL=http://sentry:9000 KAFKA_BROKER=kafka:9092 SOME_APP_ID=testid SOME_APP_KEY=testkey go run main.go
-
-	config2, err := config.FromEnv()
+	configEnv, err := config.FromEnv()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(config2)
+	fmt.Println(configEnv)
 }
